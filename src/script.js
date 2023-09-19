76cf05025aab54d2fcfd864a429c4eb09890dbab
Md Munna Mia <mdmunnamia987@gmail.com>
2023-09-19 20:52:34 +0600

    adding hero section

---
+++ src/script.js
@@ -0,0 +1,20 @@
+// const userSetting = document.querySelector("#userSetting");
+// const userBtn = document.querySelector("#userIcon");
+
+// userBtn.addEventListener("click", ()=>{
+//     let step = true;
+//     if(step===true){
+//         userSetting.classList.remove("invisible");
+//         userSetting.classList.add("visible");
+//         step = false;
+//     }
+//     else{
+//         console.log("click");
+//         userSetting.classList.remove("visible");
+//         userSetting.classList.add("invisible");
+//         step = true;
+//     }
+    
+    
+
+// })
